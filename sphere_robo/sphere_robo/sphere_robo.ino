a40834ae7f69b9a32a27a78ef1785d33e6f55e67
kenbishi <kenbiship@gmail.com>
2017-09-03 22:58:44 +0900

    RN4020通信試験

--- sphere_robo/sphere_robo/sphere_robo.ino
+++ sphere_robo/sphere_robo/sphere_robo.ino
@@ -1,9 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial BLE(18,19); //RX,TX
+#define CMD_MLDP  4
 void setup() {
   // put your setup code here, to run once:
+  pinMode(CMD_MLDP,OUTPUT);
+  digitalWrite(CMD_MLDP,LOW);
+  Serial.begin(9600);
+  BLE.begin(9600);
+  Serial.println("Sphere robo start");
 
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(BLE.available()){
+    Serial.write(BLE.read());
+  }
 }
